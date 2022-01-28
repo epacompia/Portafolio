0acd023b35508f88df86635c59f5d651a5fdf2a5
eduardopacompialopez <eduardopacompialopez@gmail.com>
2022-01-27 21:04:20 -0500

    COMPLETO HASTA EL VIDEO 43

--- Portafolio/Portafolio/Controllers/HomeController.cs
+++ Portafolio/Portafolio/Controllers/HomeController.cs
@@ -1,27 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IRepositorioProyectos repositorioProyectos;
+
+        public HomeController( IRepositorioProyectos repositorioProyectos)
         {
-            _logger = logger;
+  
+            this.repositorioProyectos = repositorioProyectos;
+
         }
 
         public IActionResult Index()
-        {  
-            return View();
+        {
+           
+
+
+            var proyectos =repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+            
+            var modelo = new HomeIndexViewModel() {
+                Proyectos = proyectos ,                
+
+            };
+            return View(modelo);
         }
 
-        public IActionResult Privacy()
+
+       
+
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
+
+
+        [HttpGet]
+        public IActionResult Contacto()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
